e8cb466f54d6a21f6c77c72bb2aa8f571c154f03
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-16 12:30:10 -0600

    Added default values and icon for button

--- src/components/see-all-button/see-all-button.js
+++ src/components/see-all-button/see-all-button.js
@@ -2,13 +2,28 @@ import React from "react";
 
 import styles from "./see-all-button.module.css";
 
-const SellAllButton = ({ count = null, link }) => (
-  <div classNam={styles.buttonBox}>
-    <a href={link} className={styles.buttonLink}>
-      <span>See all {count}</span>
-      <span>&#8594;</span>
-    </a>
-  </div>
-);
+const SellAllButton = ({
+  count = null,
+  link = null,
+  position = null,
+  bottom = 1,
+  right = 2,
+  zIndex = 11
+}) => {
+  const buttonStyles = {
+    position: `${position}`,
+    bottom: `${bottom}rem`,
+    right: `${right}rem`,
+    zIndex: `${zIndex}`
+  };
+  return (
+    <div className={styles.buttonBox} style={buttonStyles}>
+      <a href={link} className={styles.buttonLink}>
+        <span className={styles.buttonText}>See all {count}</span>
+        <span className={styles.buttonIcon}>&#10230;</span>
+      </a>
+    </div>
+  );
+};
 
 export default SellAllButton;
