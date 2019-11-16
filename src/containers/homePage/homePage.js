076efe00bb91982b50435efeb9b5aad76579aa04
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-16 11:30:40 -0600

    Renamed landing page to landing section

--- src/containers/homePage/homePage.js
+++ src/containers/homePage/homePage.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 import styles from "./homePage.module.css";
-import LandingPage from "../../components/landingPage/landingPage";
-
-import SmallCard from "../../components/smallCard/smallCard.js";
+import LandingSection from "../../components/landingSection/landingSection";
 
 class HomePage extends React.Component {
   constructor() {
@@ -14,9 +12,9 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.homePage}>
-        <LandingPage
+        <LandingSection
           imageUrl={`https://images.unsplash.com/photo-1568896597888-d674b61e7067?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80`}
-        ></LandingPage>
+        />
       </div>
     );
   }
