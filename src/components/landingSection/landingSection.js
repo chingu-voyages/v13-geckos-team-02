9ec3ca8732ab288e21ed6ac7784c14a29b81721b
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-20 17:30:42 -0600

    Added the header component

--- src/components/landingSection/landingSection.js
+++ src/components/landingSection/landingSection.js
@@ -12,7 +12,7 @@ const LandingSection = ({ imageUrl }) => {
     backgroundSize: "cover",
     position: "absolute",
     width: "100%",
-    height: "90vh",
+    height: "95vh",
     filter: "blur(10px)",
     opacity: "1"
   };
@@ -23,20 +23,22 @@ const LandingSection = ({ imageUrl }) => {
       {/* Backgroun providing gradient dark cover on the landing section */}
       <div className={styles.landingCover}></div>
       {/* Container for the big card */}
-      <div className={styles.bigSection}>
-        <BigCard
-          imageUrl={imageUrl}
-          name="Terminator:dark fate"
-          rating="7.6"
-          restriction="16+"
-          genre="action, sci-fi"
-        />
-      </div>
-      {/* Container for the small cards */}
-      <div className={styles.smallSection}>
-        <SmallCard name="the king" imageUrl={imageUrl} />
-        <SmallCard name="the king" imageUrl={imageUrl} />
-        <SmallCard name="the king" imageUrl={imageUrl} />
+      <div className={styles.cardsContainer}>
+        <div className={styles.bigSection}>
+          <BigCard
+            imageUrl={imageUrl}
+            name="Terminator:dark fate"
+            rating="7.6"
+            restriction="16+"
+            genre="action, sci-fi"
+          />
+        </div>
+        {/* Container for the small cards */}
+        <div className={styles.smallSection}>
+          <SmallCard name="the king" imageUrl={imageUrl} />
+          <SmallCard name="the king" imageUrl={imageUrl} />
+          <SmallCard name="the king" imageUrl={imageUrl} />
+        </div>
       </div>
     </div>
   );
--- src/containers/homePage/homePage.js
+++ src/containers/homePage/homePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styles from "./homePage.module.css";
+import Header from "../../components/header/header";
 import LandingSection from "../../components/landingSection/landingSection";
 
 class HomePage extends React.Component {
@@ -12,6 +13,7 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.homePage}>
+        <Header />
         <LandingSection
           imageUrl={`https://images.unsplash.com/photo-1568896597888-d674b61e7067?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80`}
         />
