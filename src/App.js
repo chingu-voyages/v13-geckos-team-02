1893ec3b13d4b1dee7a792077dcac98f1db2c1de
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-22 06:58:17 -0600

    Made header component universal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import Header from "./components/header/header";
 import HomePage from "./containers/homePage/homePage";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <HomePage />
     </div>
   );
--- src/containers/homePage/homePage.js
+++ src/containers/homePage/homePage.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import styles from "./homePage.module.css";
-import Header from "../../components/header/header";
 import LandingSection from "../../components/landingSection/landingSection";
 import CategorySection from "../../components/categorySection/categorySection";
 
@@ -14,7 +13,6 @@ class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.homePage}>
-        <Header />
         <LandingSection
           imageUrl={`https://images.unsplash.com/photo-1568896597888-d674b61e7067?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80`}
         />
