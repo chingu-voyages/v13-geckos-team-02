84491b1a1edc1e01e2a9a5794c17a2c1e6bda5e0
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-22 07:33:48 -0600

    Styled movies page body

--- src/containers/moviesPage/moviesPage.js
+++ src/containers/moviesPage/moviesPage.js
@@ -5,12 +5,15 @@ import styles from "./moviesPage.module.css";
 import PortraitCard from "../../components/portraitCard/portraitCard";
 
 const MoviesPage = () => {
+  const range = [1, 2, 3, 4, 5, 5, 6, 61, 2, 2, 3, 3, 3];
   return (
     <div className={styles.moviesPage_container}>
       <h1>Latest movies</h1>
       <div className={styles.moviesPage_genrebar}></div>
       <div className={styles.moviesPage_body}>
-        <PortraitCard />
+        {range.map(() => (
+          <PortraitCard />
+        ))}
       </div>
       <div className={styles.moviesPage_footer}></div>
     </div>
