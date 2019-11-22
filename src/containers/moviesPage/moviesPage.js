4b61e3a05c594d4d33dcf7f2020c585ebe680d62
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-22 07:47:12 -0600

    Added the page navigation buttons

--- src/containers/moviesPage/moviesPage.js
+++ src/containers/moviesPage/moviesPage.js
@@ -15,7 +15,15 @@ const MoviesPage = () => {
           <PortraitCard />
         ))}
       </div>
-      <div className={styles.moviesPage_footer}></div>
+      <div className={styles.moviesPage_footer}>
+        <button>Prev</button>
+        <button className={styles.moviesPage_footer_button_active}>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>Next</button>
+      </div>
     </div>
   );
 };
