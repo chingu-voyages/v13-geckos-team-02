93fc330dcb849ac25b51bdd5218f849739ef64ce
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-11-24 16:48:19 -0600

    Added and styled category in side bar

--- src/containers/moviePage/moviePage.js
+++ src/containers/moviePage/moviePage.js
@@ -7,6 +7,8 @@ import Overview from "../../components/overview/overview";
 import SmallCard from "../../components/smallCard/smallCard";
 const content =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit";
+const name = "Terminator:dark fate";
+const categories = ["action", "sci-fi", "thriller", "drama"];
 
 const MoviePage = () => {
   return (
@@ -14,11 +16,22 @@ const MoviePage = () => {
       <div className={styles.moviePage}>
         <div className={styles.moviePage_sideBar_container}>
           <div className={styles.sideBar_content_container}>
+            {/* Movie Poster */}
             <SmallCard
               imageUrl={`https://images.unsplash.com/photo-1568896597888-d674b61e7067?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1355&q=80`}
-              //     width={"80%"}
               height={"150px"}
             />
+            {/* Movie Name */}
+            <h2 className={styles.movieName}>{name}</h2>
+            {/* Movie Category */}
+            <div className={styles.movieCategory_box}>
+              <h3>Category</h3>
+              <div>
+                {categories.map(category => (
+                  <span className={styles.movieCategory}>{category}</span>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
         <div className={styles.moviePage_content_container}>
