476ad9e5718155260cbee61a658db6667c47b08d
Joshua Okoro <20374806+joshtru@users.noreply.github.com>
2019-12-13 15:29:10 -0600

    Now fetch data on series page based on navigation

--- src/pages/seriesPage/seriesPage.js
+++ src/pages/seriesPage/seriesPage.js
@@ -3,93 +3,150 @@ import { compose } from "redux";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-// IMPORTING TYPES
-import SeriesTypes from "../../redux/series/series.types";
 // IMPORTING REDUX ACTIONS
-import { getOnTheAirSeriesStart } from "../../redux/series/series.action";
+import {
+  getSeriesAiringTodayStart,
+  getOnTheAirSeriesStart,
+  getTopRatedSeriesStart,
+  getTrendingSeriesStart,
+  getPopularSeriesStart
+} from "../../redux/series/series.action";
+// IMPORTING SELECTORS
+import { selectSeries } from "../../redux/series/series.selector";
+import { selectCurrentPage } from "../../redux/appConfig/appConfig.selector";
 // IMPORTING COMPONENTS
 import PortraitCard from "../../components/portraitCard/portraitCard";
 // import ToggleButton from "../../components/toggleButton/toggleButton";
-import withSpinner from "../../components/withSpinner/withSpinner";
+// import withSpinner from "../../components/withSpinner/withSpinner";
 import Pagination from "../../components/pagination/pagination";
 // IMPORTING STYLES
 import styles from "./seriesPage.module.css";
 
-const SeriesPage = ({
-  series,
-  match,
-  history,
-  currentPage,
-  paginationRange,
-  setPagination,
-  fetchOnAirSeries
-}) => {
-  //   const [seriesType, setSeriesType] = useState("nowPlayingMovies");
-  //   const [seriesTypeTitle, setSeriesTypeTitle] = useState("Now Playing Movies");
-  //   useEffect(() => {
-  //     console.log(match.params);
-  //     history.push(`/${match.url}/${currentPage}`);
-  //     // fetchNowPlayingMovies();
-  //     // switch (params.type_path) {
-  //     //   case "now_playing":
-  //     //     setMoviesType("nowPlayingMovies");
-  //     //     fetchNowPlayingMovies();
-  //     //     setMoviesTypeTitle("Now Playing Movies");
-  //     //     break;
-  //     //   case "trending":
-  //     //     setMoviesType("trendingMovies");
-  //     //     setMoviesTypeTitle("Trending Movies");
-  //     //     break;
-  //     //   case "popular":
-  //     //     setMoviesType("popularMovies");
-  //     //     setMoviesTypeTitle("Popular Movies");
-  //     //     break;
-  //     //   case "top_rated":
-  //     //     setMoviesType("topRatedMovies");
-  //     //     setMoviesTypeTitle("Top Rated Movies");
-  //     //     break;
-  //     //   case "upcoming":
-  //     //     setMoviesType("upcomingMovies");
-  //     //     setMoviesTypeTitle("Upcoming Movies");
-  //     //     break;
-  //     //   default:
-  //     //     return;
-  //     // }
-  //   }, [match.params, match.url, currentPage, history]);
-  //   const newMovies = movies[moviesType] ? movies[moviesType] : [];
-  const newMovies = fetchOnAirSeries;
-  return (
-    <div className={styles.seriesPage_container}>
-      <h1>{"moviesTypeTitle"}</h1>
-      <Pagination
-        currentPage={currentPage}
-        paginationRange={paginationRange}
-        totalPages={newMovies.total_pages}
-        setPagination={setPagination}
-      />
-      <div className={styles.seriesPage_body}>
-        {newMovies.length !== 0
-          ? newMovies.results.map(movie => (
-              <PortraitCard key={movie.id} movie={movie} toPage={"series"} />
-            ))
-          : null}
+class SeriesPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seriesCategoryTitle: "Now Playing Movies",
+      seriesCategory: "airingTodaySeries",
+      url: this.props.match.params.category
+    };
+  }
+  componentDidMount() {
+    this.handleDataFetching();
+  }
+  async shouldComponentUpdate(nextProps, nextState) {
+    const {
+      match: {
+        params: { category }
+      }
+    } = await nextProps;
+    const { url } = await nextState;
+    if (category !== url) {
+      await this.setState({ url: category });
+      await this.handleDataFetching();
+      return await true;
+    } else {
+      return await false;
+    }
+  }
+  handleDataFetching = () => {
+    const {
+      history,
+      fetchSeriesAiringToday,
+      fetchOnTheAirSeries,
+      fetchTopRatedSeries,
+      fetchTrendingSeries,
+      fetchPopularSeries,
+      match: { params },
+      currentPage
+    } = this.props;
+    // history.push(`/${match.url}/${currentPage}`);
+    console.log(currentPage);
+
+    switch (params.category) {
+      case "airing_today":
+        fetchSeriesAiringToday();
+        this.setState({
+          seriesCategoryTitle: "Series Airing Today",
+          seriesCategory: "airingTodaySeries"
+        });
+        break;
+      case "on_the_air":
+        fetchOnTheAirSeries();
+        this.setState({
+          seriesCategoryTitle: "Series On The Air",
+          seriesCategory: "onTheAirSeries"
+        });
+        break;
+      case "top_rated":
+        fetchTopRatedSeries();
+        this.setState({
+          seriesCategoryTitle: "Top Rated Series",
+          seriesCategory: "topRatedSeries"
+        });
+        break;
+      case "trending":
+        fetchTrendingSeries();
+        this.setState({
+          seriesCategoryTitle: "Trending Series",
+          seriesCategory: "trendingSeries"
+        });
+        break;
+      case "popular":
+        fetchPopularSeries();
+        this.setState({
+          seriesCategoryTitle: "Popular Series",
+          seriesCategory: "popularSeries"
+        });
+        break;
+      default:
+        return;
+    }
+  };
+  render() {
+    const newSeries = this.props.series[this.state.seriesCategory]
+      ? this.props.series[this.state.seriesCategory]
+      : [];
+    const { currentPage, paginationRange, setPagination } = this.props;
+    const { seriesCategoryTitle } = this.state;
+    return (
+      <div className={styles.seriesPage_container}>
+        <h1>{seriesCategoryTitle}</h1>
+        <Pagination
+          currentPage={currentPage}
+          paginationRange={paginationRange}
+          totalPages={newSeries.total_pages}
+          setPagination={setPagination}
+        />
+        <div className={styles.seriesPage_body}>
+          {newSeries.length !== 0
+            ? newSeries.results.map(movie => (
+                <PortraitCard key={movie.id} movie={movie} toPage={"series"} />
+              ))
+            : null}
+        </div>
+        <Pagination
+          currentPage={currentPage}
+          paginationRange={paginationRange}
+          totalPages={newSeries.total_pages}
+        />
       </div>
-      <Pagination
-        currentPage={currentPage}
-        paginationRange={paginationRange}
-        totalPages={newMovies.total_pages}
-      />
-    </div>
-  );
-};
-const mapStateToProps = createStructuredSelector({});
+    );
+  }
+}
+const mapStateToProps = createStructuredSelector({
+  series: selectSeries,
+  currentPage: selectCurrentPage
+});
 const mapDispatchToProps = dispatch => ({
-  fetchOnAirTheSeries: (type, page) =>
-    dispatch(getOnTheAirSeriesStart(type, page))
+  fetchSeriesAiringToday: page => dispatch(getSeriesAiringTodayStart(page)),
+  fetchOnTheAirSeries: page => dispatch(getOnTheAirSeriesStart(page)),
+  fetchTopRatedSeries: page => dispatch(getTopRatedSeriesStart(page)),
+  fetchTrendingSeries: page => dispatch(getTrendingSeriesStart(page)),
+  fetchPopularSeries: page => dispatch(getPopularSeriesStart(page))
 });
 
 export default compose(
   connect(mapStateToProps, mapDispatchToProps),
-  withRouter,
-  withSpinner
+  withRouter
 )(SeriesPage);
